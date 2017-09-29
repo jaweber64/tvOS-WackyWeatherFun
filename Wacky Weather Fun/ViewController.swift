a4e6e9de62c260db68265f690f4076090f8cce18
Janet Weber <jaweber64@gmail.com>
2017-09-29 11:23:43 -0500

    Initial Commit

---
+++ Wacky Weather Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wacky Weather Fun
+//
+//  Created by Janet Weber on 9/29/17.
+//  Copyright © 2017 Weber Software Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
